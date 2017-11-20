a2651b8fc7d299c6a65380ae69020e431c714cc0
Matthias <matthias.zuber@students.hevs.ch>
2017-11-20 09:25:19 +0100

    Monday201

--- app/src/main/java/hes_so/android_project_2017/SanTour.java
+++ app/src/main/java/hes_so/android_project_2017/SanTour.java
@@ -2,15 +2,16 @@ package hes_so.android_project_2017;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.app.Activity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SanTour extends FragmentActivity implements OnMapReadyCallback {
+public class SanTour extends Activity  implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -19,7 +20,7 @@ public class SanTour extends FragmentActivity implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_san_tour);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
