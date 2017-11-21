88f093f1b421ee4bd5642b4794751aa05b662ea8
Matthias <matthias.zuber@students.hevs.ch>
2017-11-21 08:39:09 +0100

    Push tuesday

--- app/src/main/java/hes_so/android_project_2017/SanTour.java
+++ app/src/main/java/hes_so/android_project_2017/SanTour.java
@@ -1,8 +1,15 @@
 package hes_so.android_project_2017;
 
+import android.location.Criteria;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.app.Activity;
+
+import android.location.LocationManager;
+import android.content.Context;
+import android.location.Criteria;
+import android.content.Intent;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,14 +18,25 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.location.LocationListener;
+import android.location.Location;
+
+import java.security.Provider;
 
 public class SanTour extends FragmentActivity  implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+
         setContentView(R.layout.activity_san_tour);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -40,13 +58,93 @@ public class SanTour extends FragmentActivity  implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        //create a marker for sydney with the cordinates
-        LatLng sydney = new LatLng(-34, 151);
 
-        //create a marker for the sydney cordinates
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 
-        // move camera to center at the sydney pointer
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+        //LocationListener locationListener = new MyLocationListener();
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enable = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (enable)
+        {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
+
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getProviders(true).get(0);
+                try
+        {
+            Location location = locationManager.getLastKnownLocation(provider);
+//            //create a marker for sydney with the cordinates
+//            LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+//
+//            //create a marker for the sydney cordinates
+//            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//
+//            // move camera to center at the sydney pointer
+//            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//
+//           // locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+        }catch (SecurityException e)
+    {
+//        LatLng sydney = new LatLng(0,0);
+//
+//        //create a marker for the sydney cordinates
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//
+//        // move camera to center at the sydney pointer
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
+    }
+
+//    private class MyLocationListener implements LocationListener {
+//
+//        @Override
+//        public void onLocationChanged(Location loc) {
+//            editLocation.setText("");
+//            pb.setVisibility(View.INVISIBLE);
+//            Toast.makeText(
+//                    getBaseContext(),
+//                    "Location changed: Lat: " + loc.getLatitude() + " Lng: "
+//                            + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+//            String longitude = "Longitude: " + loc.getLongitude();
+//            Log.v(TAG, longitude);
+//            String latitude = "Latitude: " + loc.getLatitude();
+//            Log.v(TAG, latitude);
+//
+//        /*------- To get city name from coordinates -------- */
+//            String cityName = null;
+//            Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+//            List<Address> addresses;
+//            try {
+//                addresses = gcd.getFromLocation(loc.getLatitude(),
+//                        loc.getLongitude(), 1);
+//                if (addresses.size() > 0) {
+//                    System.out.println(addresses.get(0).getLocality());
+//                    cityName = addresses.get(0).getLocality();
+//                }
+//            }
+//            catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            String s = longitude + "\n" + latitude + "\n\nMy Current City is: "
+//                    + cityName;
+//            editLocation.setText(s);
+//        }
+//
+//        @Override
+//        public void onProviderDisabled(String provider) {}
+//
+//        @Override
+//        public void onProviderEnabled(String provider) {}
+//
+//        @Override
+//        public void onStatusChanged(String provider, int status, Bundle extras) {}
+//    }
+
 }
