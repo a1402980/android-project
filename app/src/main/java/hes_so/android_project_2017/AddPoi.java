dcdcbbf29d051aeb8f81a65f51ae4faf5f4023d8
Mikko Lerto <mikko.lerto@gmail.com>
2017-11-27 13:55:52 +0100

    Add POI and take or choose picture to add

---
+++ app/src/main/java/hes_so/android_project_2017/AddPoi.java
@@ -0,0 +1,13 @@
+package hes_so.android_project_2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPoi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_poi);
+    }
+}
