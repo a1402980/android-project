419bd801c5891bda87adb755f62459339a6f6156
Mikko Lerto <mikko.lerto@gmail.com>
2017-12-07 11:26:18 +0100

    nav works and add pod added

--- app/src/main/java/hes_so/android_project_2017/AddPod.java
+++ app/src/main/java/hes_so/android_project_2017/AddPod.java
@@ -1,108 +1,248 @@
 package hes_so.android_project_2017;
 
-import android.content.Context;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.PersistableBundle;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import java.util.Calendar;
+import java.io.IOException;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddPod.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AddPod#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddPod extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class AddPod extends AppCompatActivity implements View.OnClickListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button bSelectImage;
+    private Button bTakeImage;
+    private Button buploadImage;
+    private Button savePoi;
+    private StorageReference storageReference;
+    //a constant to track the file chooser intent
+    private static final int PICK_IMAGE_REQUEST = 2;
+    private ImageView imageView;
+    private EditText PoiName;
 
-    private OnFragmentInteractionListener mListener;
+    //a Uri object to store file path
+    private Uri filePath;
 
-    public AddPod() {
-        // Required empty public constructor
-    }
+    private String longitudeData;
+    private String latitudeData;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddPod.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddPod newInstance(String param1, String param2) {
-        AddPod fragment = new AddPod();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private float longitudeDataInt;
+    private float latitudeDataInt;
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        setContentView(R.layout.activity_add_pod);
+
+        setTitle("Add POD");
+
+        storageReference= FirebaseStorage.getInstance().getReference();
+
+        bSelectImage = (Button) findViewById(R.id.choosePicture);
+        bTakeImage = (Button) findViewById(R.id.takePicture);
+        savePoi = (Button) findViewById(R.id.savePOI);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        PoiName = (EditText) findViewById(R.id.poiName);
+
+
+
+        //getting the GPS data from SanTour class
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                longitudeData = null;
+                latitudeData = null;
+            } else {
+                longitudeData = extras.getString("longitudeData");
+                latitudeData = extras.getString("latitudeData");
+                longitudeDataInt = extras.getFloat("longitudeData");
+                latitudeDataInt = extras.getFloat("latitudeData");
+            }
+        } else {
+            latitudeData = (String) savedInstanceState.getSerializable("longitudeData");
+            longitudeData = (String) savedInstanceState.getSerializable("latitudeData");
+        }
+
+        //show the text data on the page
+        TextView longitudeView = (TextView) findViewById(R.id.longitudeText);
+        TextView latitudeView = (TextView) findViewById(R.id.latitudeText);
+        if (latitudeData != null && longitudeData != null) {
+            latitudeView.setText(latitudeData);
+            longitudeView.setText(longitudeData);
+        }
+
+    }
+
+    public void takePhoto() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePicture, 0);
+
+    }
+
+
+
+    //method to show file chooser
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+
+    //handling the image chooser activity result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        if (requestCode==0 && resultCode == RESULT_OK){
+            //     filePath = imageReturnedIntent.getData();
+
+            Bitmap bitmap = (Bitmap) imageReturnedIntent.getExtras().get("imageReturnedIntent");
+            imageView.setImageBitmap(bitmap);
+
+        }
+
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            filePath = imageReturnedIntent.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_pod, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+
+    @Override
+    public void onClick(View view) {
+        //if the clicked button is choose
+        if (view == bSelectImage) {
+            showFileChooser();
+        }
+        else if(view== bTakeImage){
+            takePhoto();
+        }
+
+        else if(view== savePoi){
+            POI poi = new POI();
+            poi.setName(((TextView) findViewById(R.id.poiName)).getText().toString());
+            poi.setDescription(((TextView) findViewById(R.id.editText4)).getText().toString());
+
+            poi.setLatLng(new LatLng(latitudeDataInt, longitudeDataInt));
+            poi.setFilePath(filePath);
+            LocalData.addPOI(poi);
+            finish();
+            onBackPressed();
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
+    public String getCurrentDate() {
+        Calendar calendar = Calendar.getInstance();
+        String strDate = calendar.getTime().toString();
+        return strDate;
+
+    }
+
+/* Move to LocalData
+    //this method will upload the file
+    private void uploadFile() {
+        //if there is a file to upload
+        if (filePath != null) {
+            //displaying a progress dialog while upload is going on
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading");
+            progressDialog.show();
+
+            StorageReference riversRef = storageReference.child( "/images/"+ PoiName.getText() + "/"+PoiName.getText() + "_" + getCurrentDate() +".jpg");
+            riversRef.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            //if the upload is successfull
+                            //hiding the progress dialog
+                            progressDialog.dismiss();
+
+                            //and displaying a success toast
+                            Toast.makeText(getApplicationContext(), "File Uploaded ", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            //if the upload is not successfull
+                            //hiding the progress dialog
+                            progressDialog.dismiss();
+
+                            //and displaying error message
+                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            //calculating progress percentage
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+
+                            //displaying percentage in progress dialog
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
+                        }
+                    });
+        }
+        //if there is not any file
+        else {
+            //you can display an error toast
+        }
+    }
+    */
+
+    public void cancelOnClick(View v) {
+        finish();
+        onBackPressed();
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putString("LONGITUDEDATA", latitudeData);
+        outState.putString("LATITUDDATA",longitudeData);
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+
+
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        latitudeData = (String) savedInstanceState.getString("LONGITUDEDATA");
+        longitudeData = (String) savedInstanceState.getString("LATITUDDATA");
+
+        TextView longitudeView = (TextView) findViewById(R.id.longitudeText);
+        TextView latitudeView = (TextView) findViewById(R.id.latitudeText);
+        if (latitudeData != null && longitudeData != null) {
+            latitudeView.setText(latitudeData);
+            longitudeView.setText(longitudeData);
+        }
+
     }
 }
