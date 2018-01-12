2ff18f61f056423fa5e36b47ba373d592329e185
Mikko Lerto <mikko.lerto@gmail.com>
2018-01-12 14:45:57 +0100

    fixes

--- grp5.santour/src/main/java/hes_so/santour/SanTour.java
+++ grp5.santour/src/main/java/hes_so/santour/SanTour.java
@@ -579,7 +579,7 @@ public class SanTour extends FragmentActivity implements GoogleMap.OnMyLocationB
 
     @Override
     public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Going to your location", Toast.LENGTH_SHORT).show();
         // Return false so that we don't consume the event and the default behavior still occurs
         // (the camera animates to the user's current position).
         return false;
